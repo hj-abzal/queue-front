3643b95be11c0a93423ef6a6b2717480430731f0
abzal <abzal008@mail.ru>
2022-12-18 23:27:03 +0600

    create base

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # queue-front
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-4qds1d)
---
+++ src/pages/burger-king/burger-king.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './burger-king.module.css'
+
+
+export const BurgerKing = () => {
+    return (
+        <div className={s.wrapper}>
+
+        </div>
+    );
+};
+
