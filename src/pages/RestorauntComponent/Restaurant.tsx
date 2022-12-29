74abe88273645527a779a917956ecbddd6485a35
abzal <abzal008@mail.ru>
2022-12-30 00:35:42 +0600

    slider

--- src/pages/RestorauntComponent/Restaurant.tsx
+++ src/pages/RestorauntComponent/Restaurant.tsx
@@ -3,11 +3,13 @@ import {useDispatch, useSelector} from "react-redux";
 import s from "./Restaurant.module.css";
 import {Header} from "../../components/header/Header";
 import {OrdersType, Table} from "../../components/table/Table";
-import presentFood from "../burger-king/img/burger-food.png";
-import {SpecTitle} from "../../components/SpecTitle/SpecTitle";
 import {Modal} from "../../components/Modal/Modal";
 import {AppStateType} from "../../store/store";
 import {selectedElementAC} from "../../store/orders-reducer";
+import {Advertisement} from "../../components/SpecTitle/Advertisement";
+import ph1 from '../../assets/img/test1.jpg';
+import ph2 from '../../assets/img/test2.jpg';
+import ph3 from '../../assets/img/test3.jpg';
 
 type RestaurantPropsType = {
     name: string,
@@ -18,7 +20,11 @@ export const Restaurant = (props: RestaurantPropsType) => {
     const orders = useSelector<AppStateType, OrdersType[]>(state => state.orders.orders)
     const idOfSelectedElement = useSelector<AppStateType, string>(state => state.orders.idOfSelectedElement)
     const [isOpen, setIsOpen] = useState<boolean>(false)
-
+    const img = [
+        {id: '1', img: ph1},
+        {id: '2', img: ph2},
+        {id: '3', img: ph3},
+    ]
 
     const onClickHandlerOfSelectedElement = (id: string) => {
       dispatch(selectedElementAC(id))
@@ -27,9 +33,8 @@ export const Restaurant = (props: RestaurantPropsType) => {
         <div className={s.wrapper}>
             <Header title={props.name} img={props.img} setIsOpen={setIsOpen}/>
             <Table orders={orders}
-                   idOfSelectedElement={idOfSelectedElement}
-                   img={presentFood}/>
-            <SpecTitle title={'RANDOM TEXT'}/>
+                   idOfSelectedElement={idOfSelectedElement}/>
+            <Advertisement img={img}/>
             <Modal
                 onClickHandlerOfSelectedElement={onClickHandlerOfSelectedElement}
                 orders={orders}
