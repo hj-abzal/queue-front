d3cc3dc9a5e1157f8de2e1066a37c35c0cfd2688
abzal <abzal008@mail.ru>
2023-01-07 06:23:53 +0600

    changed to hash

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
 import './index.css'
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 )
