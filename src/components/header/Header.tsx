3cb43275598c1e8188e1689c2412f8049267bbd6
maraatov <aldiyar0609@gmail.com>
2023-01-12 02:48:29 +0600

    finally_updated2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from "./Header.module.css";
-import { AiOutlineShoppingCart } from 'react-icons/ai';
+
 
 type BurgerLogoType = {
     title: string;
@@ -17,8 +17,7 @@ export const Header = (props: BurgerLogoType) => {
                     <img src={props.img} className={s.img}/>
                 </div>
                 <div className={s.shop}>
-                    {/*<button onClick={()=>{props.setIsOpen(true)}} className={s.button}>Выбрать заказ</button>*/}
-                    <AiOutlineShoppingCart className={s.icon} onClick={()=>{props.setIsOpen(true)}}/>
+                    <button onClick={()=>{props.setIsOpen(true)}} className={s.button}>Выбрать заказ</button>
                 </div>
             </div>
         </div>
