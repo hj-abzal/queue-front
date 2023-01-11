2f73b62636e6c5e52240ec83c5a159c385d88916
maraatov <aldiyar0609@gmail.com>
2023-01-12 01:42:00 +0600

    finally

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from "./Header.module.css";
-import click from './../../assets/img/click.png'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 type BurgerLogoType = {
     title: string;
@@ -15,14 +15,11 @@ export const Header = (props: BurgerLogoType) => {
             <div className={s.logoBell}>
                 <div className={s.flex}>
                     <img src={props.img} className={s.img}/>
-                    <div className={s.title}>{props.title}</div>
                 </div>
-                <button onClick={()=>{props.setIsOpen(true)}} className={s.button}>Выбрать заказ</button>
-                {/*<div className={s.flex}>*/}
-                {/*    <img src={props.clickBtn} className={s.modalButton}*/}
-                {/*            onClick={()=>{props.setIsOpen(true)}}>*/}
-                {/*    </img>*/}
-                {/*</div>*/}
+                <div className={s.shop}>
+                    {/*<button onClick={()=>{props.setIsOpen(true)}} className={s.button}>Выбрать заказ</button>*/}
+                    <AiOutlineShoppingCart className={s.icon} onClick={()=>{props.setIsOpen(true)}}/>
+                </div>
             </div>
         </div>
     );
--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -11,27 +11,47 @@ export type TableType = {
 export const Table = (props: TableType) => {
     return (
         <div className={s.main}>
-            <div className={s.blockTable1}>
-                <div className={s.blockTable2}>
-                    <div className={s.orders}>
-                        <div className={s.notDone}>Готовится</div>
-                        <div className={s.done}>Готово</div>
-                    </div>
-                    <div className={s.table}>
-                        <div className={s.numbersTrue_scroll}>
-                            {props.orders.map((t) => {
-                                if (!t.is_ready) return <div key={t.id}
-                                                             className={props.idOfSelectedElement === t.id ? `${s.selectedButton} ${s.true}` : s.true}>{t.key}</div>
-                            })}
-                        </div>
-                        <div className={s.numbersFalse_scroll}>
-                            {props.orders.map((t) => {
-                                if (t.is_ready) return <div key={t.id} className={s.false}>{t.key}</div>
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {/*<div className={s.blockTable1}>*/}
+            {/*    <div className={s.blockTable2}>*/}
+            {/*        <div className={s.orders}>*/}
+            {/*            <div className={s.notDone}>Готовится</div>*/}
+            {/*            <div className={s.done}>Готово</div>*/}
+            {/*        </div>*/}
+            {/*        <div className={s.table}>*/}
+            {/*            <div className={s.numbersTrue_scroll}>*/}
+            {/*                {props.orders.map((t) => {*/}
+            {/*                    if (!t.is_ready) return <div key={t.id}*/}
+            {/*                                                 className={props.idOfSelectedElement === t.id ? `${s.selectedButton} ${s.true}` : s.true}>{t.key}</div>*/}
+            {/*                })}*/}
+            {/*            </div>*/}
+            {/*            <div className={s.numbersFalse_scroll}>*/}
+            {/*                {props.orders.map((t) => {*/}
+            {/*                    if (t.is_ready) return <div key={t.id} className={s.false}>{t.key}</div>*/}
+            {/*                })}*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <table>
+                <tr className={s.flex}>
+                    <th className={s.check}>Готовится</th>
+                    <th className={s.check}>Готово</th>
+                </tr>
+                <tr className={s.flex}>
+                    <td className={s.numbersTrue_scroll}>
+                        {props.orders.map((t) => {
+                        if (!t.is_ready) return <td key={t.id}
+                                                    className={props.idOfSelectedElement === t.id ? `${s.selectedButton} ${s.true}` : s.true}>{t.key}</td>
+                    })}
+                    </td>
+                    <td className={s.numbersFalse_scroll}>
+                        {props.orders.map((t) => {
+                            if (t.is_ready) return <td key={t.id}
+                                                        className={props.idOfSelectedElement === t.id ? `${s.selectedButton} ${s.true}` : s.true}>{t.key}</td>
+                        })}
+                    </td>
+                </tr>
+            </table>
         </div>
     );
 };
